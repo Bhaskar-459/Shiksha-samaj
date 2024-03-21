7f3dbc7aa6c01bd004ab9aa7050551aa9d1080e1
Bhaskar <92926142+Bhaskar-459@users.noreply.github.com>
2024-03-21 07:56:39 +0530

    removed routes

--- routes/student.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/profile', (req, res) => {
-    res.sendFile(path.join(__dirname+'./profile.html'));
-});
-module.exports = router;
